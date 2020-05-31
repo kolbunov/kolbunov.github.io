ecc47dbbf1378de5550f9005b207456a90ff7f65
Даниил <daniil.kolbunov@gmail.com>
2020-05-31 20:00:42 +0300

    Create updated.js

---
+++ scripts/updated.js
@@ -0,0 +1,28 @@
+const desiredRepo = "kolbunov.github.io";
+const dateTagClass = ".date";
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function()
+{
+if (this.readyState == 4 && this.status == 200)
+{
+	let repos = JSON.parse(this.responseText);
+
+	repos.forEach((repo)=>{
+	if (repo.name == desiredRepo)
+	{
+		var lastUpdated = new Date(repo.updated_at);
+		var date = lastUpdated.getUTCDate();
+		var month = lastUpdated.getUTCMonth() + 1;
+		var year = lastUpdated.getUTCFullYear();
+		var hours = lastUpdated.getUTCHours();
+		var minute = lastUpdated.getUTCMinutes();
+		var second = lastUpdated.getUTCSeconds();
+		var test = lastUpdated.getTimezoneOffset();
+		$(dateTagClass).text(`Last Updated: ${date}.${month}.${year} ${hours}:${minute}:${second} ${test}`);
+	}
+	});
+}
+};
+xhttp.open("GET", "https://api.github.com/users/kolbunov/repos", true);
+xhttp.send();
